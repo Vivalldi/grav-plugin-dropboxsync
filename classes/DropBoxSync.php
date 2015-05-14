ca5f18277df9fb9af265fa11f2229f111b790d01
Sommerregen <sommerregen@users.noreply.github.com>
2015-05-14 17:44:12 +0200

    Refactored code and fixed typos

---
+++ classes/DropBoxSync.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * DropBox Sync
+ *
+ * Helper class to sync data with your DropBox account.
+ *
+ * Licensed under MIT, see LICENSE.
+ */
+
+namespace Grav\Plugin\DropBoxSync;
+
+use Grav\Common\Grav;
+use Grav\Common\GravTrait;
+
+/**
+ * DropBox Sync
+ *
+ * Helper class to sync your data with your DropBox account.
+ */
+class DropBoxSync
+{
+	/**
+     * @var DropBoxSync
+     */
+	use GravTrait;
+
+    protected $api;
+
+    public function __construct(array $credentials = [])
+    {
+       require_once(dirname(__DIR__).'/vendor/DropPHP/DropboxClient.php');
+
+       $this->api = $dropbox = new \DropboxClient($credentials, 'en');
+
+
+       $file = dirname(__DIR__).'/vendor/DropPHP/README.md';
+       $this->upload([$file]);
+    }
+
+    public function upload($files)
+    {
+        $this->handle_dropbox_auth();
+        foreach ($files as $key => $this_file)
+        {
+            if ( file_exists($this_file) )
+            {
+                try {
+                    $upload_name=$this_file;
+                    $result="<pre>";
+                    $result.="\r\n\r\n\<b>Uploading $upload_name:</b>\r\n";
+                    $meta=$this->api->UploadFile($this_file);
+                    $result.=print_r($meta,true);
+                    $result.="\r\n done!";
+                    $result.="</pre>";
+
+                    $result.='<span style=color:green">File successfully uploaded to you Dropbox! </span>';
+                } catch(Exception $e) {
+                    $result.='<span style="color: red">Error: ' . htmlspecialchars($e->getMessage()) . '</span>';
+                }
+            }
+        }//end foreach
+
+        $backup_folder=dirname(__FILE__).'/backup/'."dropbox_results.txt"; ;
+        $myFile = $backup_folder;
+        $fh = fopen($myFile, 'w') or die("can't open file");
+        fwrite($fh, $result);
+        fclose($fh);
+
+        return $result;
+    }//end function
+
+    public function store_token($token, $name)
+    {
+        file_put_contents("tokens/$name.token", serialize($token));
+    }
+
+    public function load_token($name)
+    {
+        if(!file_exists("tokens/$name.token")) return null;
+        return @unserialize(@file_get_contents("tokens/$name.token"));
+    }
+
+    public function delet_token($name)
+    {
+        @unlink("tokens/$name.token");
+    }
+
+    public function handle_dropbox_auth()
+    {
+        $access_token= $this->load_token("access");
+        if(!empty($access_token)){
+            $this->api->SetAccessToken($access_token);
+        }
+        elseif(!empty($_GET['auth_callback']))
+        {
+            $request_token = $this->load_token($_GET['oauth_token']);
+            if(empty($request_token)) die ('Request token not found!');
+            $access_token = $this->api->GetAccessToken($request_token);
+            $this->store_token($access_token, "access");
+            $this->delete_token($_GET['oauth_token']);
+        }
+
+        if(!$this->api->IsAuthorized())
+        {
+            $return_url = "http://".$_SERVER['HTTP_HOST'].$SERVER['SCRIPT_NAME']."?auth_callback=1";
+            $auth_url=$this->api->BuildAuthorizeUrl($return_url);
+            $request_token=$this->api->GetRequestToken();
+            $this->store_token($request_token, $request_token['t']);
+            die("Authentication required. <a href='$auth_url'>Click here.</a>");
+        }
+    }
+}
--- dropboxsync.php
+++ dropboxsync.php
@@ -1,106 +1,85 @@
 <?php
+/**
+ * DropBox Sync v1.0.0
+ *
+ * This plugin syncs data with your DropBox account.
+ *
+ * Licensed under MIT, see LICENSE.
+ *
+ * @package     DropBox Sync
+ * @version     1.0.0
+ * @link        <https://github.com/Vivalldi/grav-plugin-dropboxsync>
+ * @author      Tyler Cosgrove <mail@domain.com>
+ * @author      Benjamin Regler <sommerregen@benjamin-regler.de>
+ * @copyright   2015, Tyler Cosgrove and Benjamin Regler
+ * @license     <http://opensource.org/licenses/MIT>            MIT
+ */
+
 namespace Grav\Plugin;
 
-use Grav\Common\Data;
 use Grav\Common\Plugin;
-use Grav\Common\Uri;
-use Grav\Common\Page\Pages;
 use RocketTheme\Toolbox\Event\Event;
 
-//require_once("vendor/dropPHP/DropboxClient.php");
-include(__DIR__.'/vendor/dropPHP/DropboxClient.php');
+use Grav\Plugin\DropBoxSync\DropBoxSync;
 
-        $dropbox = new DropboxClient(array(
-            'app_key' => $this->config->get('plugins.dropbox.app_key'),
-            'app_secret' => $this->config->get('plugins.dropbox.app_secret'),
-            'app_full_acces' => false,
-        ),'en');
-
-
+/**
+ * DropBox Sync v1.0.0
+ *
+ * This plugin syncs your data with your DropBox account.
+ */
 class DropBoxSyncPlugin extends Plugin
 {
-    public static function getSubscribedEvents()
-    {
+    /** ---------------------------
+     * Private/protected properties
+     * ----------------------------
+     */
+
+    /**
+     * Instance of DropBoxSync class
+     *
+     * @var object
+     */
+    protected $backend;
+
+    /** -------------
+     * Public methods
+     * --------------
+     */
+
+    /**
+     * Return a list of subscribed events.
+     *
+     * @return array    The list of events of the plugin of the form
+     *                      'name' => ['method_name', priority].
+     */
+    public static function getSubscribedEvents() {
         return [
-            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
         ];
     }
 
+    /**
+     * Initialize configuration.
+     */
     public function onPluginsInitialized()
     {
-        DropBox_Upload("vendor/dropPHP/readme.md");
-    }
-    function DropBox_Upload($files)
-    {
-        global $dropbox;
-        handle_dropbox_auth($dropbox);
-        foreach ($files as $key => $this_file)
-        {
-            if ( file_exists($this_file) )
-            {
-                try {
-                    $upload_name=$this_file;
-                    $result="<pre>";
-                    $result.="\r\n\r\n\<b>Uploading $upload_name:</b>\r\n";
-                    $meta=$dropbox->UploadFile($this_file);
-                    $result.=print_r($meta,true);
-                    $result.="\r\n done!";
-                    $result.="</pre>";
-
-                    $result.='<span style=color:green">File successfully uploaded to you Dropbox! </span>';
-                } catch(Exception $e) {
-                    $result.='<span style="color: red">Error: ' . htmlspecialchars($e->getMessage()) . '</span>';
-                }
-            }
-        }//end foreach
-
-        $backup_folder=dirname(__FILE__).'/backup/'."dropbox_results.txt"; ;
-        $myFile = $backup_folder;
-        $fh = fopen($myFile, 'w') or die("can't open file");
-        fwrite($fh, $result);
-        fclose($fh);
-
-        return $result;
-    }//end function
-
-    function store_token($token, $name)
-    {
-        file_put_contents("tokens/$name.token", serialize($token));
-    }
-
-    function load_token($name)
-    {
-        if(!file_exists("tokens/$name.token")) return null;
-        return @unserialize(@file_get_contents("tokens/$name.token"));
-    }
-
-    function delet_token($name)
-    {
-        @unlink("tokens/$name.token");
-    }
-
-    function handle_dropbox_auth($dropbox)
-    {
-        $access_token=load_token("access");
-        if(!empty($access_token)){
-            $dropbox->SetAccessToken($access_token);
-        }
-        elseif(!empty($_GET['auth_callback']))
-        {
-            $request_token = load_token($_GET['oauth_token']);
-            if(empty($request_token)) die ('Request token not found!');
-            $access_token = $dropbox->GetAccessToken($request_token);
-            store_token($access_token, "access");
-            delete_token($_GET['oauth_token']);
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
         }
 
-        if(!$dropbox->IsAuthorized())
-        {
-            $return_url = "http://".$_SERVER['HTTP_HOST'].$SERVER['SCRIPT_NAME']."?auth_callback=1";
-            $auth_url=$dropbox->BuildAuthorizeUrl($return_url);
-            $request_token=$dropbox->GetRequestToken();
-            store_token($request_token, $request_token['t']);
-            die("Authentication required. <a href='$auth_url'>Click here.</a>");
+        if ($this->config->get('plugins.dropboxsync.enabled')) {
+            require_once(__DIR__.'/classes/DropBoxSync.php');
+
+            // Get API key and API secret
+            $credentials = [
+                'app_key' => $this->config->get('plugins.dropboxsync.app.key'),
+                'app_secret' => $this->config->get('plugins.dropbox.app.secret'),
+                'app_full_access' => false,
+            ];
+
+            // Initialize DropBoxSync class
+            $this->backend = new DropBoxSync($credentials);
         }
     }
 }
